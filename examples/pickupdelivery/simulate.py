315e2c1304b67e14a4b4165120e0996b62554b0e
MK <eng.mk@msn.com>
2020-10-02 17:33:52 +0200

    Update simulate.py

--- examples/pickupdelivery/simulate.py
+++ examples/pickupdelivery/simulate.py
@@ -12,24 +12,26 @@ def model_dynamics(x,u):
     pos_x = x[0]
     pos_y = x[1]
     battery = x[2]
-    input = u[0]
+    input = int(u[0])
     
-    is_charging = (pos_x >= 6.5 and pos_x <= 8.5) and (pos_y >= 3.5 and pos_y <= 4.5)
-    if is_charging:
-        if input == 0:      # stay
-            pos_x = pos_x
-        elif input == 1:    # left
-            pos_x = pos_x - 1.0
-        elif input == 2:    # up
-            pos_y = pos_y + 1.0
-        elif input == 3:    # right
-            pos_x = pos_x + 1.0
-        elif input == 4:    # down
-            pos_y = pos_y - 1.0
-        else:
-            pass
-        
-        battery += 1.0
+    if input == 0:      # stay
+        pass
+    elif input == 1:    # left
+        pos_x = pos_x - 1.0
+    elif input == 2:    # up
+        pos_y = pos_y + 1.0
+    elif input == 3:    # right
+        pos_x = pos_x + 1.0
+    elif input == 4:    # down
+        pos_y = pos_y - 1.0
+    else:
+        pass
+    
+    is_charging = (x[0] >= 6.5 and x[0] <= 8.5) and (x[1] >= 3.5 and x[1] <= 4.5)
+    if is_charging:        
+        battery += 20.0
+        if battery > 99.0:
+            battery = 99.0;        
     else:
         battery -= 1.0
     
@@ -41,11 +43,12 @@ def main():
         400,                    				# screen hight
         "Pickup-Delivery Drone Example",      	# screen title
         model_dynamics,         				# dynamics function of the model
-        1.0,        				            # sampling period to be used in computing the next states
+        0.1,        				            # sampling period to be used in computing the next states
         "pickupdelivery.cfg",          			# the config file oof the problem
         "pickupdelivery.mdf",          			# the controller file of the problem
-        "vehicle.png",          				# an image file to represent the model
-        0.035,                   				# scale factor of the model image
+        "drone.png",          				    # an image file to represent the model
+        0.07,                       			# scale factor of the model image
+        False,                                  # simulate dimension 3
         False,                                  # is model_dynamics ODE ?
         ["lowbattery", "fullbattery"]           # APs to skip from drawing
     )
