bd127b39123ee294c45f03352655b4549567a716
M. Khaled (MBP) <eng.mk@msn.com>
2021-03-01 21:15:49 -0700

    mods

--- build.sh
+++ build.sh
@@ -9,13 +9,14 @@ rm -rf kernel-pack/$KERNEL_NAME
 rm -rf build
 
 # rebuild owl if needed
-ifneq ($(wildcard ./kernel-driver/lib/ltl2dpa/owl/.*),)
-	$(info OWL folder is found and we assume it is installed.)
+if [ -d "./kernel-driver/lib/ltl2dpa/owl" ] 
+then
+    echo "OWL folder is found and we assume it is installed." 
 else
-    $(info OWL folder not found and we install it ...)
+    echo "OWL folder not found and we install it ..."
     cd ./kernel-driver/lib/ltl2dpa/
     sh install-owl.sh
-endif
+fi
 
 # building ...
 mkdir -p build
